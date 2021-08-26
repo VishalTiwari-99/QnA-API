a13ad3b4b04534bfc349db27aa70b077236c23a0
Vishal Tiwari <vishaltiwari1999mos@gmail.com>
2021-08-26 18:25:57 +0530

    modified models

--- API/models.py
+++ API/models.py
@@ -14,18 +14,14 @@ class Question(models.Model):
     user = models.ForeignKey('auth.User', on_delete=models.SET_NULL, null=True)
     date = models.DateField(auto_now=False, auto_now_add=False, default = date.today(), null=True)
     question_english = models.TextField(null=True)
-    option_one_english = models.TextField(null=True)
-    option_two_english = models.TextField(null=True)
-    option_three_english = models.TextField(null=True)
-    option_four_english = models.TextField(null=True)
-
     question_hindi = models.TextField(null=True)
-    option_one_hindi = models.TextField(null=True)
-    option_two_hindi = models.TextField(null=True)
-    option_three_hindi = models.TextField(null=True)
-    option_four_hindi = models.TextField(null=True)
 
-    answer = models.CharField(max_length=10, choices = ANS_CHOICES, null=True)
+    option_one = models.TextField(null=True)
+    option_two = models.TextField(null=True)
+    option_three = models.TextField(null=True)
+    option_four = models.TextField(null=True)
+
+    correct_answer = models.CharField(max_length=10, choices = ANS_CHOICES, null=True)
     fresh = models.BooleanField(default=True)
     created = models.DateField(auto_now = False, auto_now_add = True, editable=False)
     updated = models.DateField(auto_now = True, auto_now_add = False, editable=False)
@@ -33,13 +29,13 @@ class Question(models.Model):
     @property
     def get_answer(self):
         if(self.answer=='1'):
-            ans = [self.option_one_english, self.option_one_hindi]
+            ans = self.option_one
         elif(self.answer=='2'):
-            ans = [self.option_two_english, self.option_two_hindi]
+            ans = self.option_two
         elif(self.answer=='3'):
-            ans = [self.option_three_english, self.option_three_hindi]
+            ans = self.option_three
         else:
-            ans = [self.option_four_english, self.option_four_hindi]
+            ans = self.option_four
         return ans
     
     def __str__(self):
@@ -59,5 +55,23 @@ class TestSlot(models.Model):
     active = models.BooleanField(default=True)
     created = models.DateField(auto_now = False, auto_now_add = True, editable=False)
     updated = models.DateField(auto_now = True, auto_now_add = False, editable=False)
+
+class UserResponse(models.Model):
+    RESPONSE_CHOICES = (
+        ('1', '1'),
+        ('2', '2'),
+        ('3', '3'),
+        ('4', '4'),
+    )
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    test = models.ForeignKey('UserTestAttempt', on_delete=models.CASCADE)
+    question = models.ForeignKey('Question', on_delete=SET_NULL, null=True)
+    response = models.CharField(max_length=10, choices = RESPONSE_CHOICES, null=True)
+
+
+class UserTestAttempt(models.Model):
+    user = models.ForeignKey('auth.User', on_delete=models.SET_NULL, null=True)
+    assigned_test = models.ForeignKey('TestSlot', on_delete=models.CASCADE)
+    score = models.FloatField(null=True)
 
 
